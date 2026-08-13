32dd719e6db7c23b087bd2b5ccda5603196a4414
cheerystal-stack <cheerystal@gmail.com>
2026-08-13 23:04:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const homeScreen = document.getElementById("homeScreen");
 const oneCardBtn = document.getElementById("oneCardBtn");
 const oneCardScreen = document.getElementById("oneCardScreen");
 const backHome = document.getElementById("backHome");
 
 oneCardBtn.addEventListener("click", () => {
+  homeScreen.classList.add("home-hidden");
   oneCardScreen.classList.add("active");
+
+  window.scrollTo(0, 0);
 });
 
 backHome.addEventListener("click", () => {
   oneCardScreen.classList.remove("active");
+  homeScreen.classList.remove("home-hidden");
+
+  window.scrollTo(0, 0);
 });
